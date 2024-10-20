4749ed6f053128e0d71281d06658168f7ba0fb47
Robin <robin.django@gmail.com>
2024-10-20 13:13:10 +0200

    Finished header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,55 @@
+import "./Header.css";
+
 const Header = () => {
   return (
-    <>Header</>
-  )
-}
+    <header>
+      <a href="/" class="logo">
+        <img
+          src="/visual-thinking-hbo-logo.svg"
+          alt="Visual Thinking Logo"
+        />
+      </a>
 
-export default Header
+      <nav>
+        <ul>
+          <li>
+            <a
+              href="/tekenmethodes"
+            >
+              Tekenmethodes
+            </a>
+          </li>
+          <li>
+            <a href="/over">
+              Over
+            </a>
+          </li>
+          <li>
+            <a
+              href="/minicursussen"
+            >
+              Minicursussen
+            </a>
+          </li>
+          <li>
+            <a href="/kennisclips">
+              Kennisclips
+            </a>
+          </li>
+          <li>
+            <a href="/artikelen">
+              Artikelen
+            </a>
+          </li>
+          <li>
+            <a href="/tekenruimte">
+              Tekenruimte
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
